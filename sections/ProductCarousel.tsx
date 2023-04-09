bfdfb7547230d8d9fc0febb4cf7a1b9239fa5788
fernandocandeiatorres <fernandotorrescandeia@gmail.com>
2023-04-09 17:20:10 -0300

    opa

--- sections/ProductCarousel.tsx
+++
@@ -1,9 +0,0 @@
-import ProductCarousel, {
-  Props,
-} from "$store/components/product/ProductCarousel.tsx";
-
-function ProductCarouselSection(props: Props) {
-  return <ProductCarousel {...props} />;
-}
-
-export default ProductCarouselSection;
